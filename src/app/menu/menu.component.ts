33aa904f57d4efc58ed143063dc491307bcd172e
Armando27 <armandoesk17@gmail.com>
2023-12-07 10:19:03 -0600

    Modificacion Servicios

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,16 +7,15 @@ import { Component, Input } from '@angular/core';
 })
 export class MenuComponent {
   @Input() items: { src: string, alt: string, platillo:string, preci: string }[] = [ 
-    { src: './assets/images/carta/cam-diabla.jpg', alt: 'Descripción de la imagen 1',platillo:'Camarones Diabla',preci:'210'},
-  { src: './assets/images/carta/cam-emp.jpg', alt: 'Descripción de la imagen 2' ,platillo:    'Camarones Empanizados',preci:'210'},
-  { src: './assets/images/carta/cam-momia.jpg', alt: 'Descripción de la imagen 3' ,platillo:  'Camarones Momia',preci:'250'},
-  { src: './assets/images/carta/ceviche-pez.jpg', alt: 'Descripción de la imagen 4' ,platillo:'Ceviche de Pezacado',preci:'60'},
-  { src: './assets/images/carta/ceviche-cam.jpg', alt: 'Descripción de la imagen 5' ,platillo:'Ceviche de Camaron',preci:'75'},
-  { src: './assets/images/carta/ceviche-pulpo.jpg', alt:'Descripción de la imagen 6',platillo:'Ceviche de pulpo',preci:'100'},
-  { src: './assets/images/carta/fil-emp.jpg', alt: 'Descripción de la imagen 7' ,platillo:    'Filete Empanizado',preci:'130'},
-  { src: './assets/images/carta/ostiones.jpg', alt: 'Descripción de la imagen 8' ,platillo:   'Ostiones en su concha',preci:'75'},
-  { src: './assets/images/carta/pulpos-brasa.jpg', alt: 'Descripción de la imagen 9',platillo:'Pulpo a las Brazas',preci:'300'},
-  { src: './assets/images/carta/sopa-mar.jpg', alt: 'Descripción de la imagen 10' ,platillo:'Sopa de Mariscos',preci:'250'}];
+    { src: './assets/images/carta/Habitacion-normal.jpg', alt: 'Descripción de la imagen 1',platillo:'Habitacion Estandar',preci:'810'},
+  { src: './assets/images/carta/Habitacion-premium.jpeg', alt: 'Descripción de la imagen 2' ,platillo:    'Habitacion Premium',preci:'2100'},
+  { src: './assets/images/carta/Suite_presidencial.png', alt: 'Descripción de la imagen 3' ,platillo:  'Suite',preci:'3200'},
+  { src: './assets/images/carta/gimnasio.jpg', alt: 'Descripción de la imagen 4' ,platillo:'Gimnasio',preci:'Incluido'},
+  { src: './assets/images/carta/salajuegos.jpg', alt: 'Descripción de la imagen 5' ,platillo:'Sala de juegos',preci:'Incluido'},
+  { src: './assets/images/carta/spa.jpg', alt:'Descripción de la imagen 6',platillo:'Spa',preci:'Incluido'},
+  { src: './assets/images/carta/piscina.png', alt: 'Descripción de la imagen 7' ,platillo:    'Piscina',preci:'Incluido'},
+  { src: './assets/images/carta/bebidas-alimentos.jpg', alt: 'Descripción de la imagen 8' ,platillo:   'Alimentos y bebidas',preci:'Incluido'},
+  ];
   @Input() columns = 5;
   @Input() price: {preci:string}[]=[
   {preci:'210'},
